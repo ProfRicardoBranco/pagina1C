3901f7a428c4e597814b1cccf657552e75087acd
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-11-13 07:49:44 -0300

    styles.css

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.getElementById('abrirPlayer').addEventListener('click', function() {
+    document.getElementById('player').style.display = 'flex';
+  });
+  
+  document.getElementById('fecharPlayer').addEventListener('click', function() {
+    document.getElementById('player').style.display = 'none';
+  });
+  
