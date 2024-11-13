e0e25525199cc6aab4f18063b8f4c7212b853998
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-11-13 07:53:44 -0300

    styles.css

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // script.js
 
-document.getElementById('abrirPlayer').addEventListener('click', function() {
-    document.getElementById('player').style.display = 'flex';
+// Seleciona todos os links das imagens para abrir o player
+const abrirPlayerLinks = document.querySelectorAll('.abrirPlayer');
+
+// Seleciona o elemento do player e do iframe
+const player = document.getElementById('player');
+const playerIframe = document.getElementById('playerIframe');
+const fecharPlayer = document.getElementById('fecharPlayer');
+
+// Adiciona o evento de click nos links para abrir o player flutuante
+abrirPlayerLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    const videoId = this.getAttribute('data-video-id');
+    // Modifica o src do iframe para o vídeo clicado
+    playerIframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    // Exibe o player flutuante
+    player.style.display = 'flex';
   });
-  
-  document.getElementById('fecharPlayer').addEventListener('click', function() {
-    document.getElementById('player').style.display = 'none';
-  });
-  
+});
+
+// Adiciona o evento de fechar o player
+fecharPlayer.addEventListener('click', function() {
+  // Limpa o iframe e fecha o player
+  playerIframe.src = '';
+  player.style.display = 'none';
+});
